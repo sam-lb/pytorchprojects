36b159355ae0136de48c6c6b2554fdc9ba2ac9bc
sam-lb <samlbrunacini@gmail.com>
2024-04-20 13:27:22 -0400

    finish first page

--- zero.py
+++ zero.py
@@ -6,6 +6,10 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+
+
+TRAIN_MODEL = False
 
 
 training_data = datasets.FashionMNIST(
@@ -104,9 +108,42 @@ def test(dataloader, model, loss_fn):
     print(f"Test error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
-epochs = 5
-for t in range(epochs):
-    print(f"Epoch {t+1}\n --------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
-print("Done!")
+if TRAIN_MODEL:
+    epochs = 5
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n --------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer)
+        test(test_dataloader, model, loss_fn)
+    print("Done!")
+    
+    torch.save(model.state_dict(), "model.pth")
+    print("Saved PyTorch model state to model.pth")
+
+# load model
+model = NeuralNetwork().to(device)
+model.load_state_dict(torch.load("model.pth"))
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    x = x.to(device)
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predictedː "{predicted}", Actual: "{actual}"')
+
+# show image
+plt.imshow(x.permute(1, 2, 0))
+plt.show()
