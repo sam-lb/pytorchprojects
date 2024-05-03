7c7326e228cb47952e604e9df4404c3035784a12
sam-lb <samlbrunacini@gmail.com>
2024-05-03 09:11:17 -0400

    fix eigth page, delete model weights

--- seven.py
+++ seven.py
@@ -4,7 +4,7 @@ import torchvision.models as models
 model = models.vgg16(weights="IMAGENET1K_V1")
 torch.save(model.state_dict(), "model_weights.pth")
 
-model = models.vg166()
+model = models.vgg16()
 model.load_state_dict(torch.load("model_weights.pth"))
 model.eval()
 
