a4280fdef33b9cd1e11142161d3db579f1354b56
sam-lb <samlbrunacini@gmail.com>
2024-05-25 00:28:59 -0400

    implement simulation window

--- canon/main.py
+++ canon/main.py
@@ -1,2 +1,35 @@
+import pygame
+
+pygame.init()
+WIDTH, HEIGHT = 200, 200
+TITLE = "canon shooter"
+MAX_FPS = 30
+BACKGROUND_COLOR = (255, 255, 255)
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+pygame.display.set_caption(TITLE)
+pygame.key.set_repeat(100, 50)
+clock = pygame.time.Clock()
 
 
+running = True
+while running:
+
+    screen.fill(BACKGROUND_COLOR)
+    
+    pygame.display.flip()
+    clock.tick(MAX_FPS)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            break
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+                break
+        elif event.type == pygame.VIDEORESIZE:
+            WIDTH, HEIGHT = event.w, event.h
+            screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+                                    
+pygame.quit()
